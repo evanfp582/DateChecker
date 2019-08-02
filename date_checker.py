ea0fad4d9146d406caccad9d4e512d7757924dde
Evan Fisher-Perez <evanfp582@gmail.com>
2019-08-01 20:07:42 -0400

    1st Update

--- date_checker.py
+++ date_checker.py
@@ -3,11 +3,13 @@ import sys
 def leapYearFunction():
     print('this is a leap year that begins on ' + str(firstDay))
     desiredDateLeap()
+    print(day+" "+ month)
 
     
 def noLeapYearFunction():
     print('this is a year that begins on ' + str(firstDay))
     desiredDate()
+    print(day+" "+ month)
 
 
 def desiredDate():
@@ -21,30 +23,47 @@ def desiredDate():
         print('what day of that month would you like to know')
         daysInMonth=year.get(month)
         day=input().lower()
-        if (int(day) <= daysInMonth)& (int(day)>0):
-            print('great!')
-            break
+        try:
+            if (int(day) <= daysInMonth)& (int(day)>0):
+                print('great!')
+                break
+        except ValueError:
+            pass
         print('That day doesnt fit within the month')
     print('you picked a date number within the month')
         
         
             
-#This is also broken for a reason im not sure of   
+
 def desiredDateLeap():
     while 1==1:
         print('what month would you like to know')
-        leapMonth=input().lower()
-        if (leapMonth in leapYear.keys())==True:
+        month=input().lower()
+        if (month in leapYearDict.keys())==True:
             print('one morestep done')
             break
-#
+    while 1==1:
+        print('what day of that month would you like to know')
+        daysInMonth=leapYearDict.get(month)
+        day=input().lower()
+        try:
+            if (int(day) <= daysInMonth)& (int(day)>0):
+                print('great!')
+                break
+        except ValueError:
+            pass
+        print('That day doesnt fit within the month')
+    print('you picked a date number within the month')
+
 
 
 #Dictionary of months and the number of days
 year={'january':31, 'february':28, 'march': 31, 'april':30, 'may':31, 'june':30, 'july':31,'august':31, 'september':30, 'october':31, 'november':30, 'december':31}
-leapYear={'january':31, 'february':29, 'march': 31, 'april':30, 'may':31, 'june':30, 'july':31,'august':31, 'september':30, 'october':31, 'november':30, 'december':31}
+leapYearDict={'january':31, 'february':29, 'march': 31, 'april':30, 'may':31, 'june':30, 'july':31,'august':31, 'september':30, 'october':31, 'november':30, 'december':31}
 
 days=['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
+day=""
+month=""
 ##############################################
 
 
@@ -76,5 +95,4 @@ if leapYear=='yes':
     leapYearFunction()
 if leapYear=='no':
     noLeapYearFunction()
-
 
